c904927dec6752e4c2f5acb5c3d7972304bd4952
alokkumar07 <aloknith07@gmail.com>
2023-02-12 16:42:10 +0530

    youtube bugs

---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ButtonList from './ButtonList'
+import ViedoContainer from './ViedoContainer'
+const MainContainer = () => {
+  return (
+    <div className='col-span-11'>
+        <ButtonList/>
+        <ViedoContainer/>
+    </div>
+  )
+}
+
+export default MainContainer
